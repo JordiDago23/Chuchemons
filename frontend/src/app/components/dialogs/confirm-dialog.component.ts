d0b131872f308a64fab182557b9608eb4919052e
Miquelsimoz24 <miquelsimoz@gmail.com>
2026-04-10 15:48:30 +0200

    accesibilidad
    mejoras de accesibilidad

--- frontend/src/app/components/dialogs/confirm-dialog.component.ts
+++ frontend/src/app/components/dialogs/confirm-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.css']
 })
-export class ConfirmDialogComponent {
+export class ConfirmDialogComponent implements AfterViewChecked {
   @Input() isVisible = false;
   @Input() title = 'Confirmación';
   @Input() message = '¿Estás seguro?';
@@ -17,6 +17,20 @@ export class ConfirmDialogComponent {
   
   @Output() confirm = new EventEmitter<void>();
   @Output() cancel = new EventEmitter<void>();
+
+  @ViewChild('confirmBtn') confirmBtn!: ElementRef<HTMLButtonElement>;
+
+  private focusSet = false;
+
+  ngAfterViewChecked() {
+    if (this.isVisible && this.confirmBtn && !this.focusSet) {
+      this.confirmBtn.nativeElement.focus();
+      this.focusSet = true;
+    }
+    if (!this.isVisible) {
+      this.focusSet = false;
+    }
+  }
 
   onConfirm() {
     this.confirm.emit();
